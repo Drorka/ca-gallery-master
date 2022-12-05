f97689ceb0286d3febc7899f2aa695b617cf17b7
Drorka~ <drorpa4@gmail.com>
2022-12-05 14:51:32 +0200

    render portfolio grid

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-console.log('Starting up');
+'use strict'
+console.log('Starting up')
+
+var gProjs = [
+  {
+    id: 'bookshop',
+    name: 'Book Shop',
+    title: 'Flourish and Blotts Magical Book Shop',
+    desc: 'Enter the magical shop in Diagon Alley',
+    url: 'https://drorka.github.io/book-shop/',
+    publishedAt: 1669903200000,
+    labels: ['table', 'filter', 'crudl', 'render'],
+  },
+  {
+    id: 'minesweeper',
+    name: 'Mine Sweeper',
+    title: 'Sweep Those Mines',
+    desc: 'Open all cells without exploding!',
+    url: 'https://drorka.github.io/Minesweeper/',
+    publishedAt: 1669557600000,
+    labels: ['matrix', 'timer', 'render'],
+  },
+  {
+    id: 'touchnums',
+    name: 'Touch Nums',
+    title: "Touch 'em Nums",
+    desc: 'Tap on the numbers by order',
+    url: 'https://drorka.github.io/Touch-nums/',
+    publishedAt: 1669125600000,
+    labels: ['matrix', 'timer', 'render'],
+  },
+  {
+    id: 'pacman',
+    name: 'Pacman',
+    title: 'The All-Time Classic',
+    desc: 'Eat all the foods, avoid all the ghosts',
+    url: 'https://drorka.github.io/InPic/',
+    publishedAt: 1668693600000,
+    labels: ['matrix', 'flow', 'render'],
+  },
+]
+
+function getGProjs() {
+  return gProjs
+}
